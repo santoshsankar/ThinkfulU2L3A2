b570e356e1d397fcc5da26441317de0639a477a3
santoshsankar <santoshsankar@gmail.com>
2014-09-07 12:03:33 -0400

    Initial Commit

---
+++ CarManufacturing/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CarManufacturing
+//
+//  Created by Santosh Sankar on 9/7/14.
+//  Copyright (c) 2014 Santosh Sankar. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
